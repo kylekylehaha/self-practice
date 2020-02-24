c9e47e133c189694284b0ebf75c2212ca4557044
Kyle <kyle@Kylede-MacBook-Pro.local>
2020-02-24 22:43:20 +0800

    still have problem, but finish mergesort

---
+++ c/merge_sort.c
@@ -0,0 +1,136 @@
+//merge sort with single & double linked_list
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct node{
+    int value;
+    struct node* prev;
+    struct node* next; 
+}List;
+
+void dump(List* head){
+    if(!head || !head->next)
+        return;
+
+    printf("%d",head->value);
+    List* temp = head->next;
+    while(temp){
+        printf(" ,%d",temp->value);
+        temp = temp->next;
+    }
+    printf("\n");
+    return;
+}
+
+void create_List(List** ref, int value){
+    List* node = malloc(sizeof(List));
+    node->value = value;
+    node->next = node->prev = NULL;
+    if(!(*ref)){
+        *ref = node;
+    }
+    else{
+        node->next = *ref;
+        (*ref)->prev = node;
+        (*ref) = node;
+    }
+    return;
+}
+
+
+List* mergeSort_single(List* start){
+    if(!start || !start->next)
+        return start;
+    
+    List *left, *right;
+    left = start;
+    right = start->next;
+    left->next = NULL;
+
+    left = mergeSort_single(left);
+    right = mergeSort_single(right);
+
+    for (List* merge = NULL; left || right;){
+        if (!right || (left && left->value <= right->value)){
+            if (!merge){
+                merge = start = left;
+            }
+            else{
+                merge->next = left;
+                merge = merge->next;
+            }
+            left = left->next;
+        }
+        else{
+            if (!merge){
+                merge = start = right;
+            }
+            else{
+                merge->next = right;
+                merge = merge->next;
+            }
+            right = right->next;
+        }
+    }
+    return start;
+}
+
+List* mergeSort_double(List* start){
+    if (!start || !start->next)
+        return start;
+    List *left, *right;
+    left = start;
+    right = start->next;
+    left->next = NULL;
+
+    left = mergeSort_double(left);
+    right = mergeSort_double(right);
+
+    for (List* merge = NULL; left || right;){
+        if (!right || (left && left->value <= right->value)){
+            if (!merge){
+                merge = start = left;
+            }
+            else{
+                merge->next = left;
+                merge->prev = left->prev;
+                merge = merge->next;
+            }
+            left = left->next;
+        }
+        else{
+            if(!merge){
+                merge = start = right;
+            }
+            else{
+                merge->next = right;
+                merge->prev = right->prev;
+                merge = merge->next;
+            }
+            right = right->next;
+        }
+    }
+    return start;
+}
+
+int main(){
+    int i;
+    List *list = NULL;
+    List *list1, *list2;
+    srand(time(NULL));
+    for (i=0;i<20;i++){
+        create_List(&list, rand()%100);
+    }
+    printf("before mergesort\n");
+    dump(list);
+    list1 = mergeSort_single(list);
+    //list2 = mergeSort_double(list);
+    printf("list:\n");
+    dump(list);
+    printf("list1:\n");
+    dump(list1);
+    printf("\n");
+    printf("list2:\n");
+    //dump(list2);
+}
