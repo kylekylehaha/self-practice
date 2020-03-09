07f3d1f0cefbbfa452615901f1184c64b572d6af
kylekylehaha <kyle870216@gmail.com>
2020-03-10 00:11:57 +0800

    Add fib_fast_doubling with clz

--- c/fib_test/show_test.gp
+++ c/fib_test/show_test.gp
@@ -5,7 +5,9 @@ set title 'runtime'
 set term png enhanced font 'Verdana,10'
 
 set output 'runtime.png'
+set key left top
 
 plot [:][:] \
 'test_fd.txt' using 1:2 with linespoints linewidth 2 title 'fast doubling', \
-'test_fseq.txt' using 1:2 with linespoints linewidth 2 title 'sequence'   
+'test_fd_clz.txt' using 1:2 with linespoints linewidth 2 title 'fast doubling clz', \
+'test_fseq.txt' using 1:2 with linespoints linewidth 2 title 'sequence'
--- c/fib_test/test.c
+++ c/fib_test/test.c
@@ -2,6 +2,76 @@
 #include <stdlib.h>
 #include <time.h>
 
+unsigned int clz(long long k)
+{
+	unsigned int n;
+	long long clz = k;
+
+	if (k == 0)
+		return 0;
+	
+	if (clz <= 0x00000000FFFFFFFF) {
+        n += 32;
+        clz <<= 32;
+    }
+    if (clz <= 0x0000FFFFFFFFFFFF) {
+        n += 16;
+        clz <<= 16;
+    }
+    if (clz <= 0x00FFFFFFFFFFFFFF) {
+        n += 8;
+        clz <<= 8;
+    }
+    if (clz <= 0x0FFFFFFFFFFFFFFF) {
+        n += 4;
+        clz <<= 4;
+    }
+    if (clz <= 0x3FFFFFFFFFFFFFFF) {
+        n += 2;
+        clz <<= 2;
+    }
+    if (clz <= 0x7FFFFFFFFFFFFFFF) {
+        n += 1;
+        clz <<= 1;
+    }
+
+	return n;
+
+}
+
+unsigned long long fib_fast_doubling_clz(long long k)
+{
+    unsigned int digit = 0;
+    int saved = k;
+    while (k) {
+        digit++;
+        k /= 2;
+    }
+    k = saved;
+	
+	unsigned int n = clz(k);
+	k << n;
+
+    unsigned long long a, b;
+    a = 0;
+    b = 1;
+    for (unsigned int i = digit; i > 0; i--) {
+        unsigned long long t1, t2;
+        t1 = a * (2 * b - a);
+        t2 = b * b + a * a;
+        a = t1;
+        b = t2;
+        if (k & (1 << (i - 1))) {
+            t1 = a + b;
+            a = b;
+            b = t1;
+            // k &= ~(1 << (i - 1));
+        }
+    }
+    return a;
+}
+
+
 unsigned long long fib_fast_doubling(long long k)
 {
     unsigned int digit = 0;
@@ -61,6 +131,7 @@ int main()
 {
     FILE *fd = fopen("test_fd.txt", "wb+");
 	FILE *fseq = fopen("test_fseq.txt", "wb+");
+	FILE *fd_clz = fopen("test_fd_clz.txt", "wb+");
 
 	unsigned long long result = 0;
     int offset = 92; /* TODO: try test something bigger than the limit */
@@ -89,6 +160,19 @@ int main()
 		printf("offset: %d, fib is %llu\n", i, result);
     }
 
+	printf("use fib_fd_clz now\n");
+    for (int i = 0; i <= offset; i++) {
+        struct timespec start, end;
+        
+		clock_gettime(CLOCK_REALTIME, &start);
+		result = fib_fast_doubling_clz((long long)i);
+        clock_gettime(CLOCK_REALTIME, &end);
+        fprintf(fd_clz, "%d %u\n", i, diff_in_ns(start, end));
+		
+		printf("offset: %d, fib is %llu\n", i, result);
+    }
+
+	fclose(fd_clz);
     fclose(fd);
     fclose(fseq);
     return 0;
