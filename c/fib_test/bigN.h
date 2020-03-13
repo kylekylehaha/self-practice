381ada34fa9b6f5c3c663eb13a796afd64ad06b1
kylekylehaha <kyle870216@gmail.com>
2020-03-13 21:35:39 +0800

    Delete test file for linux kernel fibdrv

--- c/fib_test/bigN.h
+++
@@ -1,9 +0,0 @@
-#define BASE 100000000
-#define bit_per part 8
-#define part_num 14
-
-typedef struct BigN {
-	unsigned long long  part[part_num];
-	unsigned int kernel_t;
-	unsigned int user_t;
-} bigN;
