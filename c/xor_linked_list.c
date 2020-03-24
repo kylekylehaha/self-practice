fdce4c30111e798580d3ffe5f7ef524c293c6091
Kyle <kyle@Kylede-MacBook-Pro.local>
2020-03-24 18:47:08 +0800

    Add optimizing merge sort

--- c/xor_linked_list.c
+++ c/xor_linked_list.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 10
+#define LIST_LEN 20
+#define EXP_TIME 10
 
 typedef struct __list list;
 struct __list {
@@ -33,6 +34,27 @@ void delete_list(list *l) {
         free(l);
         l = next;
     }
+}
+
+void print(list *l)
+{
+    if (!l) {
+        printf("list is not exist now\n");
+        return;
+    }
+    printf("list: ");
+    list *next = l->addr;
+    while(l) {
+        list *tmp;
+        printf("%3d", l->data);
+        if (next) {
+            tmp = XOR(next->addr, l);
+        }
+        l = next;
+        next = tmp;
+    }
+    printf("\n");
+    return;
 }
 
 list *merge(list *left, list *right)
@@ -73,27 +95,6 @@ list *merge(list *left, list *right)
     return start;
 }
 
-void print(list *l)
-{
-    if (!l) {
-        printf("list is not exist now\n");
-        return;
-    }
-    printf("list: ");
-    list *next = l->addr;
-    while(l) {
-        list *tmp;
-        printf("%3d", l->data);
-        if (next) {
-            tmp = XOR(next->addr, l);
-        }
-        l = next;
-        next = tmp;
-    }
-    printf("\n");
-    return;
-}
-
 list *sort(list *start)
 {
     if (!start || !start->addr)
@@ -132,15 +133,77 @@ list *insertion_sort(list *head)
     return sorted;
 }
 
+static void split(list *src, list **left, list **right, int *left_len, int *right_len, int thres)
+{
+    list *fast = src, *prev_fast = NULL, *next_fast;
+    list *slow = src, *prev_slow = NULL, *next_slow;
+    int len = 0;
+    while (fast && XOR(fast->addr, prev_fast)) {
+        /* fast = fast->next->next */
+        next_fast = XOR(fast->addr, prev_fast);
+        fast = XOR(fast, next_fast->addr);
+        prev_fast = next_fast;
+        /* slow = slow->next */
+        next_slow = XOR(slow->addr, prev_slow);
+        prev_slow = slow;
+        slow = next_slow;
+        len++;
+    }
+    *left = src;
+    if (!src) {
+        *right = NULL;
+    } else if (fast) {
+        /* list is odd */
+        list *next = XOR(prev_slow, slow->addr);
+        next->addr = XOR(slow, next->addr);
+        *right = next;
+        slow->addr = XOR(slow->addr, next);
+        *left_len = len;
+        *right_len = len + 1;
+    } else {
+        /* list is even */
+        prev_slow->addr = XOR(prev_slow->addr, slow);
+        slow->addr = XOR(prev_slow, slow->addr);
+        
+        *right = slow;
+        *left_len = *right_len = len;
+    }
+    return;
+}
+
+list *op_merge_sort(list *start, int list_len, int thres)
+{
+    if (!start || !start->addr)
+        return start;
+    if (list_len < thres) {
+        return insertion_sort(start);
+    }
+
+    list *head = start;
+    list *left, *right;
+    int left_len = 0, right_len = 0;
+    split(head, &left, &right, &left_len, &right_len, thres);
+
+    left = op_merge_sort(left, left_len, thres);
+    right = op_merge_sort(right, right_len, thres);
+    
+    return merge(left, right);
+}
+
 int main(){
-    list **start;
     srand((unsigned)time(NULL));
 
-    for(int i = 0;i < SIZE;i++){
-        insert_node(start, rand()%100);
+    for (int i = 0;i <2; i++) {
+        list **start ;
+        *start = NULL;
+        for (int j = 0;j < LIST_LEN; j++) {
+            insert_node(start, rand()%100);
+        }
+        printf("before sort\n");
+        print(*start);
+        *start = op_merge_sort(*start, LIST_LEN, 6);
+        print(*start);
+        delete_list(*start);
     }
-    print(*start);
-    *start = insertion_sort(*start);
-    print(*start);
     return 0;
 }
